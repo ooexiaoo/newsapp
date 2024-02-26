5cd083f34a37d0bda98d089b43ddd5197e4dde36
ooexiaoo <vpaherwar@gmail.com>
2024-02-26 15:56:20 +0530

    Chapter 28

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={!imageUrl?"https://png.pngtree.com/png-vector/20221117/ourmid/pngtree-breaking-news-vector-icon-illustration-logo-design-png-image_6461447.png":imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+            <a rel="norederrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
       </div>
