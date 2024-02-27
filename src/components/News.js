c8ece7af04e797e8bf7664835ce2d11ed5d178f7
ooexiaoo <vpaherwar@gmail.com>
2024-02-28 02:07:49 +0530

    Chapter 32

--- src/components/News.js
+++ src/components/News.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 export class News extends Component {
   static defaultPorps = {
-    country: 'in',
+    country: "in",
     pageSize: 6,
-    category: 'general'
-  }
+    category: "general",
+  };
 
   static propTypes = {
     name: PropTypes.string,
     pageSize: PropTypes.number,
-    category: PropTypes.string
-  }
+    category: PropTypes.string,
+  };
 
   constructor() {
     super();
@@ -27,67 +27,84 @@ export class News extends Component {
 
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fbbd9d7c93b4484eaa44c68c41f5f5e7&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     /* console.log(data); */
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading: false
+      loading: false,
     });
   }
 
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fbbd9d7c93b4484eaa44c68c41f5f5e7&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=fbbd9d7c93b4484eaa44c68c41f5f5e7&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
-    this.setState({loading:true});
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
     /* console.log(data); */
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
-      loading: false
+      loading: false,
     });
   };
   handleNextClick = async () => {
-    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fbbd9d7c93b4484eaa44c68c41f5f5e7&page=${
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${
+        this.props.category
+      }&apiKey=fbbd9d7c93b4484eaa44c68c41f5f5e7&page=${
         this.state.page + 1
       }&pageSize=${this.props.pageSize}`;
-      this.setState({loading:true});
+      this.setState({ loading: true });
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
-        loading: false
-      })
+        loading: false,
+      });
     }
   };
 
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{margin: '30px 0px'}}>💩 NEWS - Top Headlines</h1>
-        {this.state.loading && <Spinner/>}
+        <h1 className="text-center" style={{ margin: "30px 0px" }}>
+          💩 NEWS - Top Headlines
+        </h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title.slice(0, 40) : ""}
-                  description={
-                    element.description ? element.description.slice(0, 80) : ""
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title}
+                    description={element.description}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
@@ -99,7 +116,10 @@ export class News extends Component {
             &larr; Previous
           </button>
           <button
-            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)}
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
             type="button"
             className="btn btn-dark"
             onClick={this.handleNextClick}
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
-      <div className='my-3'>
+      <div className="my-3">
         <div className="card">
-          <img src={!imageUrl?"https://png.pngtree.com/png-vector/20221117/ourmid/pngtree-breaking-news-vector-icon-illustration-logo-design-png-image_6461447.png":imageUrl} className="card-img-top" alt="..."/>
+          <img
+            src={
+              !imageUrl
+                ? "https://png.pngtree.com/png-vector/20221117/ourmid/pngtree-breaking-news-vector-icon-illustration-logo-design-png-image_6461447.png"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <a rel="norederrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+            <h5 className="card-title">
+              {title}
+              <span class="position-absolute top-0 translate-middle badge rounded-pill bg-dark" style={{left: '50%', zIndex:'1'}}>{source}</span>
+            </h5>
+            <p className="card-text">{description}</p>
+            <p class="card-text">
+              <small class="text-body-secondary">
+                By {author ? author : " Uknown "} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="norederrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
