e212a760590d58079a85892ae4e8c21433c0b7d2
ooexiaoo <vpaherwar@gmail.com>
2024-02-27 17:40:44 +0530

    Chapter 31 with errors

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 export default class App extends Component {
-  render() {
+  component() {
     return (
       <div>
+        <Router>
         <NavBar/>
-        <News pageSize={5} country="in" category="business"/>
+        <Routes>  
+        <Route exact path="/business"><News key="business" pageSize={5} country="in" category="Business" /></Route>
+        <Route exact path="/entertainment"><News key="entertainment" pageSize={5} country="in" category="Entertainment" /></Route>
+        <Route exact path="/general"><News key="general" pageSize={5} country="in" category="General"  /></Route>
+        <Route exact path="/health"><News key="health" pageSize={5} country="in" category="Health" /></Route>
+        <Route exact path="/science"><News key="science" pageSize={5} country="in" category="Science"  /></Route>
+        <Route exact path="/sports"><News key="sports" pageSize={5} country="in" category="Sports" /></Route>
+        <Route exact path="/technology"><News key="technology" pageSize={5} country="in" category="Technology" /></Route>
+        </Routes>
+        </Router>
       </div>
     )
   }
