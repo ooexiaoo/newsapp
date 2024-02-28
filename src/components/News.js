c6f0014a95ed4e5deff8a4ab4d894417bef4f28c
ooexiaoo <vpaherwar@gmail.com>
2024-02-29 03:35:15 +0530

    If I add ${this.props.apiKey} in fetch news it breaks the app while trying to fetch the news

--- src/components/News.js
+++ src/components/News.js
@@ -63,7 +63,7 @@ export class News extends Component {
 
   fetchMoreData = async () => {
     this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fbbd9d7c93b4484eaa44c68c41f5f5e7&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
